19bb2e304fb4fa050b42cbda1c628c1181c6187d
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-07-12 23:36:06 +0600

    Add header file for Powerpuff functions and colors

---
+++ powerpuff.h
@@ -0,0 +1,20 @@
+#ifndef POWERPUFF_H
+#define POWERPUFF_H
+
+// Color macros
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define BLUE "\033[1;34m"
+#define PINK "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define YELLOW "\033[1;33m"
+#define WHITE "\033[1;37m"
+#define PURPLE "\033[1;35m"
+#define RESET "\033[0m"
+
+// Function Prototypes
+void registerUser();
+int loginUser();
+void startPowerPuff_Quest();
+
+#endif
